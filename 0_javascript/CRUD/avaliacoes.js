e3798918d93834c13def955ab88844ca060866d5
gabrielmartinscp <gabrielmartinscp@hotmail.com>
2023-09-24 14:11:13 -0300

    correções nos scripts das avaliações

--- 0_javascript/CRUD/avaliacoes.js
+++ 0_javascript/CRUD/avaliacoes.js
@@ -4,15 +4,15 @@
 
 async function criar_avaliacao(id_atendimento, estrelas, texto)
 {
+    let resposta = '';
     function callback(res)
     {
-        return res;
+        resposta = res;
     }
     let query = `${config.servidor}/${config.diretorios.avaliacoes}/criar.php?idatendimento=${id_atendimento}&estrelas=${estrelas}&texto="${texto}"`;
     
-    let res = reqHTTP(query, callback);
-    await res;
-    return res.then((result) => {return result});
+    reqHTTP(query, callback);
+    return JSON.parse(resposta);
 }
 
 /*-----------------------
@@ -39,15 +39,15 @@ function ler_avaliacao(id)
 
 async function atualizar_avaliacao(id_avaliacoes, id_atendimento, estrelas, texto)
 {
+    let resposta = '';
     function callback(res)
     {
-        return res;
+        resposta = res;
     }
     let query = `${config.servidor}/${config.diretorios.avaliacoes}/atualizar.php?idavaliacoes=${id_avaliacoes}&idatendimento=${id_atendimento}&estrelas=${estrelas}&texto="${texto}"`;
     
-    let res = reqHTTP(query, callback);
-    await res;
-    return res.then((result) => {return result});
+    reqHTTP(query, callback);
+    return JSON.parse(resposta);
 }
 
 /*-----------------------
@@ -56,13 +56,13 @@ async function atualizar_avaliacao(id_avaliacoes, id_atendimento, estrelas, text
 
 async function deletar_avaliacao(id_avaliacoes)
 {
+    let resposta = '';
     function callback(res)
     {
-        return res;
+        resposta = res;
     }
     let query = `${config.servidor}/${config.diretorios.avaliacoes}/atualizar.php?idavaliacoes=${id_avaliacoes}`;
     
-    let res = reqHTTP(query, callback);
-    await res;
-    return res.then((result) => {return result});
+    reqHTTP(query, callback);
+    return JSON.parse(resposta);
 }
