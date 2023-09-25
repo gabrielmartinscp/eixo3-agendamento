16ce538151db209c0274c0a374ff38d563ed47bc
joão josé <joaojosefaleirosantos@gmail.com>
2023-09-25 20:53:37 -0300

    Second update classe agendamento

--- classes/Agendamento.php
+++ classes/Agendamento.php
@@ -3,36 +3,29 @@ namespace Easybook\classes;
 
 final class Agendamento
 {
-    private int $id_atendimentos;
-    // private int $id_horario;
-    // private int $id_cliente;
     private string $estado;
 
-    public function __construct(int $id_atendimentos)
+    public function __construct(string $estado = '', private int $id_atendimentos, private int $id_horario = -1, private int $id_cliente = -1)
     {
-        $this->id_atendimentos = $this->validar_id_atendimentos($id_atendimentos);
+        $this->estado = $this->validar_estado($estado);
     }
 
     // validação 
-
-    private function validar_id_atendimentos($id_atendimentos)
-    {
-        return $id_atendimentos;
-    }
-
-    // private function validar_id_horario($id_horario)
-    // {
-    //     return $id_horario;
-    // }
-
-    // private function validar_id_cliente($id_cliente)
-    // {
-    //     return $id_cliente;
-    // }
     
-    private function validar_estado($estado)
+    private function validar_estado(string $e): string 
     {
-        return $estado;
+        switch($e){
+            case 'solicitado':
+            case 'confirmado':
+            case 'realizado':
+            case 'cancelado':
+                  return $e;
+                  break;
+            default:
+                  return 'solicitado';
+                  break;
+         }
+
     }
 
     //get
@@ -40,18 +33,16 @@ final class Agendamento
     function getIdAtendimentos(): int
     {
         return $this->id_atendimentos;
-
     }
 
-    // function getIdHorario(): int {
+    function getIdHorario(): int {
+        return $this->id_horario;
+    }
 
-    //     return $this->id_horario;
-    // }
-
-    // function getIdCliente(): int
-    // {
-    //     return $this->id_cliente;
-    // }
+    function getIdCliente(): int
+    {
+        return $this->id_cliente;
+    }
     
     function getEstado(): string
     {
