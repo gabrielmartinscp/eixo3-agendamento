8ca6dc81bdc5f22f50defdee88279256f43b77e8
gabrielmartinscp <gabrielmartinscp@hotmail.com>
2023-10-11 06:32:44 -0300

    .

--- cadastro/cadastro.php
+++ cadastro/cadastro.php
@@ -12,7 +12,7 @@ try
 
     $conexao = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_nome, $db_usuario, $db_senha);
 
-    $registro = $conexao->prepare("INSERT INTO" . $db_tabela_dados_login_senha . "(login, senha) VALUES (:nome_usuario, :senha_criptografada)");
+    $registro = $conexao->prepare("INSERT INTO " . $db_tabela_dados_login_senha . " (login, senha) VALUES (:nome_usuario, :senha_criptografada)");
 
     $registro->bindParam(':nome_usuario', $login);
     $registro->bindParam(':senha', $senha_criptografada);
